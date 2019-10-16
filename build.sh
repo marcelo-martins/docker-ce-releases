0a3b88d8cb0b4d18f87425a0205da7fd8944433b
marcelo-martins <marcelobmartins219@hotmail.com>
2019-10-16 11:35:01 -0300

    changed version to 18

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@
 #github_version="18.09.7"
 #github_version="19.03.1"
 set -e
-github_version="19.03.3"
+github_version="18.09.7"
 ftp_version=$(cat ftp_version.txt)
 del_version=$(cat delete_version.txt)
 status=$(curl -s --head -w %{http_code} https://oplab9.parqtec.unicamp.br/pub/ppc64el/docker/version-$github_version/$sys -o /dev/null)
