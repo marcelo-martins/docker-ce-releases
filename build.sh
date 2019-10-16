d7166889d8b82ab404b8dbaa85ac4cdf83378027
marcelo-martins <marcelobmartins219@hotmail.com>
2019-10-15 22:42:39 -0300

    changed docker version to 19.03.3

--- build.sh
+++ build.sh
@@ -7,7 +7,8 @@
 #github_version="18.09.5"
 #github_version="18.09.6"
 #github_version="18.09.7"
-github_version="19.03.1"
+#github_version="19.03.1"
+github_version="19.03.3"
 ftp_version=$(cat ftp_version.txt)
 del_version=$(cat delete_version.txt)
 status=$(curl -s --head -w %{http_code} https://oplab9.parqtec.unicamp.br/pub/ppc64el/docker/version-$github_version/$sys -o /dev/null)
